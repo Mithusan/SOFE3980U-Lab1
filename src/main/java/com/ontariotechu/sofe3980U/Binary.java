64e3c9775c275cd8171664b20d9fd3e55ab8d4b1
DarrenConsOnTech <darren.constantine@ontariotechu.net>
2023-01-27 21:12:27 -0500

    Update Binary.java

--- src/main/java/com/ontariotechu/sofe3980U/Binary.java
+++ src/main/java/com/ontariotechu/sofe3980U/Binary.java
@@ -138,118 +138,119 @@ public class Binary
 
 	public static Binary bitWiseOr(Binary num1, Binary num2)
 	{
-		//Carry Index of first digit of each number
+		// Carry Index of first digit of each number
 		int ind1=num1.number.length()-1;
 		int ind2=num2.number.length()-1;
 
-		//INTILIZE VARIABLES
-		String answer="";//Used throughout method to carry binary value
-		int temp;//Temporary Variable
+		// INTILIZE VARIABLES
+		String answer="";// Used throughout method to carry binary value
+		int temp;// Temporary Variable
 
-		//Loops as long as either variable are larger than 0
-		//meaning more numbers remain to be looked at
-		while(ind1>=0 || ind2>=0){//Will Loop until all digits are processed
-			temp=0;//Clears temp variable after each loop
+		// Loops as long as either variable are larger than 0
+		// meaning more numbers remain to be looked at
+		while(ind1>=0 || ind2>=0){// Will Loop until all digits are processed
+			temp=0;// Clears temp variable after each loop
 
-			//Checks if any Digits remain
-			if (ind1 >= 0){//If less than 0 than no digits remain
-				//Check if the digit at the current loop iteration’s index is a 1
-				//If it happens to be a 1 then temp will carry the value 1,
-				//if not, it will carry the value 0
+			// Checks if any Digits remain
+			if (ind1 >= 0){// If less than 0 than no digits remain
+				// Check if the digit at the current loop iteration’s index is a 1
+				// If it happens to be a 1 then temp will carry the value 1,
+				// if not, it will carry the value 0
 				temp = (num1.number.charAt(ind1) == '1')? 1:temp;
-				ind1--;//Update
+				ind1--;// Update
 			}
-			if (ind2 >= 0){//If less than 0 than no digits remain
-				//Check if the digit at the current loop iteration’s index is a 1
-				//If it happens to be a 1 then temp will carry the value 1,
-				//if not, it will carry the value 0
+			if (ind2 >= 0){// If less than 0 than no digits remain
+				// Check if the digit at the current loop iteration’s index is a 1
+				// If it happens to be a 1 then temp will carry the value 1,
+				// if not, it will carry the value 0
 				temp = (num2.number.charAt(ind2) == '1')? 1:temp;
-				ind2--;//Update
+				ind2--;// Update
 			}
 
-			//If temp is 1 in this loop then answer will hold value 1, if not 0
-			//Stores current digits calculation
+			// If temp is 1 in this loop then answer will hold value 1, if not 0
+			// Stores current digits calculation
 			answer = ((temp==1)? "1":"0") + answer;
 		}
 
-		Binary result=new Binary(answer);//Create a binary object with the calculated value
-		return result;//return the value to App.java
+		Binary result=new Binary(answer);// Create a binary object with the calculated value
+		return result;// return the value to App.java
 	}
 
 	public static Binary bitWiseAnd(Binary num1, Binary num2)
 	{
-		//Carry Index of first digit of each number
+		// Carry Index of first digit of each number
 		int ind1=num1.number.length()-1;
 		int ind2=num2.number.length()-1;
 
-		//INTILIZE VARIABLES
-		String answer="";//Used throughout method to carry binary value
-		int temp1;//Temporary Variable
-		int temp2;//Temporary Variable
+		// INTILIZE VARIABLES
+		String answer="";// Used throughout method to carry binary value
+		int temp1;// Temporary Variable
+		int temp2;// Temporary Variable
 
-		//Loops as long as either variable are larger than 0
-		//meaning more numbers remain to be looked at
-		while(ind1>=0 || ind2>=0){//Will Loop until all digits are processed
-			//Clears temp variable after each loop
+		// Loops as long as either variable are larger than 0
+		// meaning more numbers remain to be looked at
+		while(ind1>=0 || ind2>=0){// Will Loop until all digits are processed
+			// Clears temp variable after each loop
 			temp1 = 0;
 			temp2 = 0;
 
-			//Checks if any Digits remain
-			if (ind1 >= 0){//If less than 0 than no digits remain
-				//Check if the digit at the current loop iteration’s index is a 1
-				//If it happens to be a 1 then temp will carry the value 1,
-				//if not, it will carry the value 0
+			// Checks if any Digits remain
+			if (ind1 >= 0){// If less than 0 than no digits remain
+				// Check if the digit at the current loop iteration’s index is a 1
+				// If it happens to be a 1 then temp will carry the value 1,
+				// if not, it will carry the value 0
 				temp1 = (num1.number.charAt(ind1) == '1')? 1:0;
-				ind1--;//Update
+				ind1--;// Update
 			}
-			if (ind2 >= 0){//If less than 0 than no digits remain
-				//Check if the digit at the current loop iteration’s index is a 1
-				//If it happens to be a 1 then temp will carry the value 1,
-				//if not, it will carry the value 0
+			if (ind2 >= 0){// If less than 0 than no digits remain
+				// Check if the digit at the current loop iteration’s index is a 1
+				// If it happens to be a 1 then temp will carry the value 1,
+				// if not, it will carry the value 0
 				temp2 = (num2.number.charAt(ind2) == '1')? 1:0;
-				ind2--;//Update
+				ind2--;// Update
 			}
 
-			//If both temp values are 1 in this loop then answer will hold value 1, if not 0
-			//Stores current digits calculation
+			// If both temp values are 1 in this loop then answer will hold value 1, if not 0
+			// Stores current digits calculation
 			answer = ((temp1==1 && temp2==1)? "1":"0") + answer;
 		}
 
-		Binary result=new Binary(answer);//Create a binary object with the calculated value
-		return result;//return the value to App.java
+		Binary result=new Binary(answer);// Create a binary object with the calculated value
+		return result;// return the value to App.java
 	}
 
 	public static Binary bitWiseMultiply(Binary num1, Binary num2)
 	{
-		//Carry Index of first digit of each number
+		// Carry index of LSB of each number
 		int ind1=num1.number.length()-1;
 		int ind2=num2.number.length()-1;
 
-		//INTILIZE VARIABLES
-		Binary result = new Binary("0"); //Create a binary object to hold calculated value
-		//If the first number is larger then value will be of the first number
-		//Else it will be the second number
+		// INTILIZE VARIABLES
+		Binary result = new Binary("0"); // Create a binary object to hold calculated value
+		//Larger number will be treated as multiplicand, and smaller number as the multiplier
 		Binary largeNum = (ind1>=ind2)? num1:num2;
-		//If the first number is equal to the calculated 'largeNum'
-		//It will be the second number if not the first number
+		//Sets smallNum based on the value set for largeNum
 		Binary smallNum = (largeNum==num1)? num2:num1;
-		//Assigns objects to variables
+
+		// Reassigns binary objects so largeNum > num1 and smallNum > num2
+		// will be needed when performing shifts
 		num1 = largeNum;
 		num2 = smallNum;
 
-		//loops until no digits remain
+		// Loops until all bits of the multiplier (smallNum) are read 
 		for(int i=smallNum.number.length()-1; i>=0; i--)
 		{
-			//If current iteration value is equal to 1
+			// If bit at current position equals 1, the value of num1 is added to the result
+			// Add function is called to perform the addition
 			if(smallNum.number.charAt(i)=='1'){
-				//Sends to add method to add larger number
-				//to result
 				result = add(result,num1);
 			}
-			//If not will add 0 to calculation for current iteration
+			// 0 is concatenated to the multiplicand to prepare for the next iteration
+			// Similar to moving from ones to tens place in decimal - an extra 0 is required
+			// before the next intermediate addition operation can be performed
 			num1.number+="0";
 		}
 
-		return result;//return the value to App.java
+		return result; // Return binary object containing product binary string
 	}
 }	
